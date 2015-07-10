962c3fae1abc2a657183f1bbbeaf668465feacea
Sammaye <sam.millman@googlemail.com>
2015-07-10 13:09:46 +0100

    Removed addoption since it now exists in yii2-mongodb

--- Query.php
+++ Query.php
@@ -8,13 +8,6 @@ use sammaye\mongoyii\MongoCursor;
 
 class Query extends BaseQuery
 {
-	public $options = [];
-
-	public function addOption($key, $value)
-	{
-		$this->options[$key] = $value;
-	}
-
 	public function each()
 	{
 		return Yii::createObject([
@@ -28,69 +21,8 @@ class Query extends BaseQuery
 		return $this->buildCursor();
 	}
 
-	/**
-	 * Builds the Mongo cursor for this query.
-	 * @param Connection $db the database connection used to execute the query.
-	 * @return \MongoCursor mongo cursor instance.
-	 */
 	public function buildCursor($db = null)
 	{
-		$cursor = $this->getCollection($db)->find($this->composeCondition(), $this->composeSelectFields());
-		if (!empty($this->orderBy)) {
-			$cursor->sort($this->composeSort());
-		}
-		$cursor->limit($this->limit);
-		$cursor->skip($this->offset);
-
-		foreach($this->options as $k => $v){
-			$cursor->addOption($k, $v);
-		}
-
-		return $cursor;
-	}
-
-	/**
-	 * Composes condition from raw [[where]] value.
-	 * @return array conditions.
-	 */
-	private function composeCondition()
-	{
-		if ($this->where === null) {
-			return [];
-		} else {
-			return $this->where;
-		}
-	}
-
-	/**
-	 * Composes select fields from raw [[select]] value.
-	 * @return array select fields.
-	 */
-	private function composeSelectFields()
-	{
-		$selectFields = [];
-		if (!empty($this->select)) {
-			foreach ($this->select as $key => $value) {
-				if (is_numeric($key)) {
-					$selectFields[$value] = true;
-				} else {
-					$selectFields[$key] = $value;
-				}
-			}
-		}
-		return $selectFields;
-	}
-
-	/**
-	 * Composes sort specification from raw [[orderBy]] value.
-	 * @return array sort specification.
-	 */
-	private function composeSort()
-	{
-		$sort = [];
-		foreach ($this->orderBy as $fieldName => $sortOrder) {
-			$sort[$fieldName] = $sortOrder === SORT_DESC ? \MongoCollection::DESCENDING : \MongoCollection::ASCENDING;
-		}
-		return $sort;
+		return parent::buildCursor($db);
 	}
 }
